0a990022e0634c029e59685682056d1dda9486d0
moniad <moni.dziedzic@gmail.com>
2024-09-28 17:19:08 +0200

    Add simple text form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,29 @@
 import Map from './components/Map';
 import Header from './components/Header';
 import React, { useState } from 'react';
+import Prompt from "./components/TextRoutePrompt";
 
 const apikey = 'B1i2fhMekYEaFkkPoVJjErKYaquAglsTyib4of2WPfE'
 
 function App() {
+    const [inputValue, setInputValue] = useState('');
+    const handleInputChange = (newValue) => {
+        // if (newValue === "#") {
+        //     return;
+        // }
+        setInputValue(newValue);
+    };
+
   return (
     <div style={{
       width: "100vw",
       height: "100vh"
     }}>
       <Header />
+        <Prompt
+            value={inputValue}
+            onChange={handleInputChange}
+        />
       <Map apikey={apikey} />
     </div>
   );
